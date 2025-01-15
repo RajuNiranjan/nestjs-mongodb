1e233db2147ba708694f4fc6694e46e4b660b165
Raju Niranjan <rajuniranjan@gmail.com>
2025-01-15 11:18:58 +0530

    added all packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.DB_URI),
-    AuthModule,
-    BookModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
